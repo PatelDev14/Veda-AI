dc88c607d00b5d45be7a6b794596a0d0cd341c1e
Dev Patel <devp1400@gmail.com>
2026-02-17 18:45:45 -0500

    Initial Commit

---
+++ Veda/VedaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VedaApp.swift
+//  Veda
+//
+//  Created by Dev Patel on 2026-02-17.
+//
+
+import SwiftUI
+
+@main
+struct VedaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
